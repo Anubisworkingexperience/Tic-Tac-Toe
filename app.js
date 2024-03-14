c3b11e738a9df0bbbd382115401545359ff9bcf9
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2024-03-14 23:26:50 +0300

    Update board and computer choice functions

--- app.js
+++ app.js
@@ -20,37 +20,52 @@ const playerTwo = {
 function game() {
     console.log('Welcome to the game of tic tac toe');
     let chooseMode = prompt(`Choose your game mode. Type "1p" to play against computer and "2p" to play 2 player game on one device`).toLowerCase();
-    let choosePlayer = prompt(`Choose "X" or "O"`).toLowerCase();
-    console.log(chooseMode, choosePlayer);
+    let chooseDifficulty = prompt('Choose game difficulty. Easy(e), medium(m), hard(h), master(m)');
+    let choosePlayer = prompt(`Choose "X" or "O"`).toUpperCase();
+
+    console.log(chooseMode, chooseDifficulty, choosePlayer);
+
     let board = gameBoard.board;
     console.log(board);
+
     let cellsUsed = gameBoard.cellsUsed;
+
     const onePlayerGame = function() {
         console.log("Game started!");
         while (cellsUsed != 9) {
-        let cell = prompt("Enter row, column of your cell. Format: row column").split(" ");
-        let row = Number(cell[0]);
-        let column = Number(cell[1]);
-        console.log(row, column);
-        
-        if (board[row-1][column-1] !== "X" && board[row-1][column-1] !== "O") {
-            choosePlayer === "x" ? board[row-1][column-1] = "X" :
-            choosePlayer === "o" ? board[row-1][column-1] = "O" :
-            console.log("Not valid value");
-            cellsUsed += 1;
-            console.log(board);
-            console.log(cellsUsed);
-        }
-        else {
-            console.log("Cell is taken already");
-            console.log(board);
-        }
-        
-        }
-    }
+            let cell = prompt("Enter row, column of your cell. Format: row column").split(" ");
+            let row = Number(cell[0]);
+            let column = Number(cell[1]);
+            console.log(row, column);
+            
+            function updateBoardArray() {
+                if (board[row-1][column-1] === "#") {
+                    board[row-1][column-1] = choosePlayer;
+                    cellsUsed += 1;
+                    console.log(board);
+                    console.log(cellsUsed);
+                }
+                else {
+                    console.log("Cell is taken already");
+                    console.log(board);
+                }
+                
+            }
 
-    const computerChoice = function() {
-        
+            if (choosePlayer === "x") {
+                updateBoardArray();
+                computerEasyChoice();
+            }
+            else {
+                computerEasyChoice();
+                updateBoardArray();
+            }
+    }
+}
+
+    const computerEasyChoice = function() {
+        //easy
+        console.log("some easy computer");
     }
 
     const twoPlayerGame = function() {
