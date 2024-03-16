8e344577666595ba28640c938433261ee135a323
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2024-03-16 18:39:32 +0300

    Add computer easy level

--- app.js
+++ app.js
@@ -21,9 +21,12 @@ function game() {
     console.log('Welcome to the game of tic tac toe');
     let chooseMode = prompt(`Choose your game mode. Type "1p" to play against computer and "2p" to play 2 player game on one device`).toLowerCase();
     let chooseDifficulty = prompt('Choose game difficulty. Easy(e), medium(m), hard(h), master(m)');
-    let choosePlayer = prompt(`Choose "X" or "O"`).toUpperCase();
+    let choosePlayer = prompt(`Choose "X" or "O"`).toLowerCase();
+    let chooseComputer;
+    choosePlayer === "x" ? chooseComputer = "o" :
+    chooseComputer = "x";
 
-    console.log(chooseMode, chooseDifficulty, choosePlayer);
+    console.log(chooseMode, chooseDifficulty, choosePlayer, chooseComputer);
 
     let board = gameBoard.board;
     console.log(board);
@@ -36,25 +39,28 @@ function game() {
             let cell = prompt("Enter row, column of your cell. Format: row column").split(" ");
             let row = Number(cell[0]);
             let column = Number(cell[1]);
+            let currentCellTaken = false;
             console.log(row, column);
             
             function updateBoardArray() {
                 if (board[row-1][column-1] === "#") {
-                    board[row-1][column-1] = choosePlayer;
+                    board[row-1][column-1] = choosePlayer.toUpperCase();
                     cellsUsed += 1;
                     console.log(board);
                     console.log(cellsUsed);
                 }
                 else {
                     console.log("Cell is taken already");
+                    currentCellTaken = true;
                     console.log(board);
                 }
-                
             }
 
             if (choosePlayer === "x") {
                 updateBoardArray();
-                computerEasyChoice();
+                if (currentCellTaken === false) {
+                    computerEasyChoice();
+                }
             }
             else {
                 computerEasyChoice();
@@ -63,9 +69,23 @@ function game() {
     }
 }
 
+function winCondition() {
+    
+}
+
     const computerEasyChoice = function() {
-        //easy
         console.log("some easy computer");
+        const randomRow = Math.round(Math.random() * 2);
+        const randomColumn = Math.round(Math.random() * 2);
+        console.log(randomRow + 1, randomColumn + 1);
+        console.log(board[randomRow][randomColumn]);
+        if (board[randomRow][randomColumn] === "#") {
+            board[randomRow][randomColumn] = chooseComputer.toUpperCase();
+            console.log(board);
+        }
+        else {
+            computerEasyChoice();
+        }
     }
 
     const twoPlayerGame = function() {
